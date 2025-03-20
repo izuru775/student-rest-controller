092c2f37baf88ce202ae3306213e335069c313f0
Isuru Adikaram <izuru775@gmail.com>
2025-03-20 19:24:49 +1100

    PostConstruct added to load the list

--- src/main/java/com/isuru/demo/rest/StudentRestController.java
+++ src/main/java/com/isuru/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.isuru.demo.rest;
 
 import com.isuru.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,25 +13,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> students;
+    // Define @PostConstruct to load the student data ... only once
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
+
+        students.add(new Student("Isuru","Adikaram"));
+        students.add(new Student("Pamodee","Panchalee"));
+        students.add(new Student("Nipuna","Adikaram"));
+    }
+
    // Define an endpoint for "/student" -return a list of students
    @GetMapping("/students")
    public List<Student> getStudents(){
-       List<Student> students = new ArrayList<>();
-
-       students.add(new Student("Isuru","Adikaram"));
-       students.add(new Student("Pamodee","Panchalee"));
-       students.add(new Student("Nipuna","Adikaram"));
 
        return students;
    }
 
    @GetMapping("/students/{studentId}")
    public Student getStudent(@PathVariable int studentId){
-       List<Student> students = new ArrayList<>();
-
-       students.add(new Student("Isuru","Adikaram"));
-       students.add(new Student("Pamodee","Panchalee"));
-       students.add(new Student("Nipuna","Adikaram"));
 
        return students.get(studentId);
    }
