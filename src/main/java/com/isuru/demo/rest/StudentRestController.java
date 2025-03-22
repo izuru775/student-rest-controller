1a7f443708738ebde5b98ea3520d57af015cfa4a
Isuru Adikaram <izuru775@gmail.com>
2025-03-22 16:17:46 +1100

    ExceptionHandler added

--- src/main/java/com/isuru/demo/rest/StudentRestController.java
+++ src/main/java/com/isuru/demo/rest/StudentRestController.java
@@ -2,6 +2,7 @@ package com.isuru.demo.rest;
 
 import com.isuru.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,6 +39,18 @@ public class StudentRestController {
         }
        return students.get(studentId);
 
+   }
+
+   // Add an Exception handler using @ExceptionHandler
+   @ExceptionHandler
+   public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+        // Create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        // Return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
    }
 
 }
